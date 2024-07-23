19cc8646f26275607ecd9aa571d9a501c6e2789e
hakanmertb <72814063+hakanmertb@users.noreply.github.com>
2024-07-23 22:33:11 +0300

    general

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:redux_boilerplate/jsonata.dart';
 
 import 'features/counter/views/counter_view.dart';
 
 class AppRoutes {
   static const String home = '/';
-  static const String auth = '/auth';
+  static const String test = '/test';
 
   static Map<String, WidgetBuilder> routes = {
     home: (context) => const CounterView(),
+    test: (context) => const TestPage(),
   };
 }
